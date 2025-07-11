0cc4340a5e012506618d1f75d77b153206cbe487
unknown <hidrabula@gmail.com>
2025-07-12 01:19:26 +0700

    add .env for next

--- src/app/constants/url.tsx
+++ src/app/constants/url.tsx
@@ -1,2 +1,2 @@
-export const BASEURL = `http://localhost:3001`;
-export const WSS = `ws://localhost:3001`;
+export const BASEURL = process.env.DOMAIN || 'http://localhost:3001';
+export const WSS = process.env.WSS || 'ws://localhost:3001';
