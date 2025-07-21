8a891ef5be3a2c23b67488524049047211851519
unknown <hidrabula@gmail.com>
2025-07-21 23:24:25 +0700

    fix: dashboard ui change,prenium ui change , feat(add): add home about website page

--- src/app/constants/url.tsx
+++ src/app/constants/url.tsx
@@ -1,3 +1,3 @@
-export const BASEURL = "http://localhost:3001";
-// export const BASEURL = 'https://student-campus.onrender.com';
+
+export const BASEURL = 'https://student-campus.onrender.com';
 export const WSS = "wss://student-campus.onrender.com";
