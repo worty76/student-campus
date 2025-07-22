d1bd72672a4fc2799ccf511d887e0a8f70182125
Worty <lethanhdat762003@gmail.com>
2025-07-22 09:22:14 +0700

    fix: rebuild

--- src/services/premiumPayment.service.ts
+++ src/services/premiumPayment.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BASEURL } from "../app/constants/url";
 import {
   PremiumPurchaseRequest,
